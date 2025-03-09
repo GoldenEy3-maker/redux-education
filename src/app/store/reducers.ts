d477cda2aa36ecc47b53272404a2a248454c2b0a
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2025-03-10 00:02:44 +0700

    fix: remove unused counter slice from root reducer

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,8 +1,6 @@
-import { counterSlice } from "@/features/counter";
 import { combineReducers } from "@reduxjs/toolkit";
 import { apiSlice } from "@/shared/api/api-slice";
 
 export const rootReducer = combineReducers({
-  [counterSlice.name]: counterSlice.reducer,
   [apiSlice.reducerPath]: apiSlice.reducer,
 });
